818c76bb3a0ee5e43f055fa83f7a4546f4a549c3
Alexis Muñoz <alexis.m16.99@gmail.com>
2020-10-27 20:49:49 -0300

    Cambio en el formulario registro

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -2,11 +2,15 @@ from django import forms
 from .models import Usuario
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+
+def agregarClaseFormControl(elementos):
+    for campo in elementos:
+        campo.field.widget.attrs['class'] = 'form-control'
+
 class inicioForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(UserCreationForm,self).__init__(*args, **kwargs)
-        for campo in self.visible_fields():
-            campo.field.widget.attrs['class'] = 'form_control'
+        agregarClaseFormControl(self.visible_fields())
     class Meta:
         model = Usuario
         fields = ('first_name','last_name','username','password1','password2','email','edad','fecha_nacimiento','genero')
