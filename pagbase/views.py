2c7806f61c0b548c544c10c0664b7e7821fa1580
crhyss <cfuentes320@gmail.com>
2020-10-29 18:42:06 -0300

    sesion areglado

--- pagbase/views.py
+++ pagbase/views.py
@@ -1,20 +1,21 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 # Create your views here.
-from .models import Producto,Categoria
-from .forms import ProductoForm,CategoriaForm
+from .models import Producto, Categoria
+from .forms import ProductoForm, CategoriaForm
+from usuarios.forms import inicioForm
 
 def paginaprincipal(request):
     formulario = AuthenticationForm()
-    usuario =AuthenticationForm()
+    usuario = AuthenticationForm()
     lista = Categoria.objects.all()
     if request.method == 'POST':
-        formulario = AuthenticationForm(data = request.POST)
+        formulario = AuthenticationForm(data=request.POST)
         if formulario.is_valid():
             username = formulario.cleaned_data['username']
             password = formulario.cleaned_data['password']
-            usuarioLogeado = authenticate(username = username,password = password)
+            usuarioLogeado = authenticate(username=username, password=password)
             if usuarioLogeado is not None:
                 login(request, usuarioLogeado)
                 if usuarioLogeado.username == 'admin':
@@ -22,40 +23,44 @@ def paginaprincipal(request):
                 else:
                     return redirect('/')
     context = {
-        'titulo':'Tiendita del Cris',
-        'formulario2':formulario,
-        'lista':lista
+        'titulo': 'Tiendita del Cris',
+        'formulario2': formulario,
+        'lista': lista
     }
     return render(
         request,
         'pagbase/principal.html',
         context
-    )   
+    )
+
+
 def agregarProducto(request):
     lista = Categoria.objects.all()
     formulario = None
     if request.method == 'POST':
-        formulario = ProductoForm(request.POST,request.FILES)
+        formulario = ProductoForm(request.POST, request.FILES)
         if formulario.is_valid():
             formulario.save()
             return redirect('/listar/')
     else:
         formulario = ProductoForm()
     context = {
-        'titulo':'Agregar Producto',
-        'formulario':formulario,
-        'lista':lista,
-    }    
+        'titulo': 'Agregar Producto',
+        'formulario': formulario,
+        'lista': lista,
+    }
     return render(
         request,
         'pagbase/agregar.html',
         context
     )
-def modificarProducto(request,id_producto):
+
+
+def modificarProducto(request, id_producto):
     productoRecibido = Producto.objects.get(pk=id_producto)
     lista = Categoria.objects.all()
     formulario = None
-    if request.method =='POST':
+    if request.method == 'POST':
         formulario = ProductoForm(request.POST, instance=productoRecibido)
         if formulario.is_valid():
             formulario.save()
@@ -63,76 +68,104 @@ def modificarProducto(request,id_producto):
     else:
         formulario = ProductoForm(instance=productoRecibido)
     context = {
-        'titulo':'Modificar Producto',
-        'formulario':formulario,
-        'lista':lista,
-    }    
+        'titulo': 'Modificar Producto',
+        'formulario': formulario,
+        'lista': lista,
+    }
     return render(
         request,
         'pagbase/modificar_t.html',
         context
     )
+
+
 def listarProducto(request):
     productos = Producto.objects.all()
     lista = Categoria.objects.all()
     context = {
-        'titulo':'Productos',
-        'productos':productos,
-        'lista':lista,
-    }    
+        'titulo': 'Productos',
+        'productos': productos,
+        'lista': lista,
+    }
     return render(
         request,
         'pagbase/listarTecnologia.html',
         context
     )
-def eliminarProducto(request,id_producto):
-    productoEliminado = Producto.objects.get(pk = id_producto)
+
+
+def eliminarProducto(request, id_producto):
+    productoEliminado = Producto.objects.get(pk=id_producto)
     productoEliminado.delete()
     return (redirect('/listar/'))
+
 
 def administracion(request):
     lista = Categoria.objects.all()
     context = {
-        'titulo':'Administrador',
-        'lista':lista,
-    }   
+        'titulo': 'Administrador',
+        'lista': lista,
+    }
     return render(
         request,
         'admin/admin.html',
         context
     )
-def lista(request,id):
-    usuario = AuthenticationForm()
+
+
+def lista(request, id):
+    productos = Producto.objects.filter(categoria_id__exact=id)
+    formulario2 = AuthenticationForm()
     lista = Categoria.objects.all()
-    productos = Producto.objects.filter(categoria_id__exact = id)
+    if request.method == 'POST':
+        formulario2 = AuthenticationForm(data=request.POST)
+        if formulario2.is_valid():
+            username = formulario2.cleaned_data['username']
+            password = formulario2.cleaned_data['password']
+            usuarioLogeado = authenticate(username=username, password=password)
+            if usuarioLogeado is not None:
+                login(request, usuarioLogeado)
+                return redirect('/')
     context = {
-        'titulo':'lista',
-        'usuario':usuario,
-        'productos':productos,
-        'lista':lista,
-        'usuario':usuario
-    }   
+        'titulo': 'lista',
+        'productos': productos,
+        'formulario2': formulario2,
+        'lista':lista
+    }
     return render(
         request,
         'pagbase/catalogoModa.html',
         context
     )
 
-def muestraProducto(request,id_producto):
+
+
+def muestraProducto(request, id_producto):
     lista = Categoria.objects.all()
-    usuario = AuthenticationForm()
     productoRecibido = Producto.objects.get(pk=id_producto)
+    formulario2 = AuthenticationForm()
+    if request.method == 'POST':
+        formulario2 = AuthenticationForm(data=request.POST)
+        if formulario2.is_valid():
+            username = formulario2.cleaned_data['username']
+            password = formulario2.cleaned_data['password']
+            usuarioLogeado = authenticate(username=username, password=password)
+            if usuarioLogeado is not None:
+                login(request, usuarioLogeado)
+                return redirect('/')
     context = {
-        'titulo':'Moda',
-        'producto':productoRecibido,
-        'usuario':usuario,
-        'lista':lista,
-    }    
+        'titulo': 'Productos',
+        'producto': productoRecibido,
+        'lista': lista,
+        'formulario2': formulario2,
+    }
     return render(
         request,
         'pagbase/datosProductos.html',
         context
     )
+
+
 def categoria(request):
     usuario = AuthenticationForm()
     lista = Categoria.objects.all()
@@ -145,12 +178,12 @@ def categoria(request):
     else:
         formulario = CategoriaForm()
     context = {
-        'titulo':'Agregar Categoria',
-        'formularioC':formulario,
-        'lista':lista,
-        'usuario':usuario,
-        
-    }    
+        'titulo': 'Agregar Categoria',
+        'formularioC': formulario,
+        'lista': lista,
+        'usuario': usuario,
+
+    }
     return render(
         request,
         'admin/categoria.html',
