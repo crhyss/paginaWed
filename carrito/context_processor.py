9daf61601e81b9dea7b01755b6767ac694a477a2
crhyss <cfuentes320@gmail.com>
2020-11-05 17:40:15 -0300

    se areglo el error de context_processor

--- carrito/context_processor.py
+++ carrito/context_processor.py
@@ -1,7 +1,6 @@
-from django.contrib.auth.models import User
 def monto_total_cart(request):
     total = 0
     if request.user.is_authenticated:
-        for key, value in request.session['cart']:
+        for key, value in request.session['cart'].items():
             total = total + (int(value['precio']) * value['cantidad'])
     return {'monto_total_cart': total}
